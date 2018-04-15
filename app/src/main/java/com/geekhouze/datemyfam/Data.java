55b90558b157f70b966941df1f261f41f33f933d
geoffie <geoff.seloana@gmail.com>
2018-04-15 21:19:10 +0200

    drawable added

--- app/src/main/java/com/geekhouze/datemyfam/Data.java
+++ app/src/main/java/com/geekhouze/datemyfam/Data.java
@@ -1,4 +1,23 @@
 package com.geekhouze.datemyfam;
 
+
 public class Data {
+
+    private String description;
+
+    private String imagePath;
+
+    public Data(String imagePath, String description) {
+        this.imagePath = imagePath;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
 }
