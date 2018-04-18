118417fe9fa68ddeef30ce8673f996a5dffc3646
geoffie <geoff.seloana@gmail.com>
2018-04-18 22:46:39 +0200

    drawable added

--- app/src/main/java/com/geekhouze/datemyfam/MainActivity.java
+++ app/src/main/java/com/geekhouze/datemyfam/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Data> array;
     private SwipeFlingAdapterView flingContainer;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
